d848be1dab0c0ffa63185592aed186fe8ce545a1
ashifujjmanRafi <ajrafi2722@gmail.com>
2021-01-13 13:19:10 +0600

    adding Strings

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+
+	public static void main(String[] args) {
+		String firstname="Ashifujjman ";
+		String lastname ="Rafi";
+		String fullname=firstname+lastname;
+		System.out.println(fullname);
+
+		
+	}
+}
