3342a02ec931d13eca4648a283ff7b789fa80508
ashifujjmanRafi <ajrafi2722@gmail.com>
2021-02-16 01:04:47 +0600

    new folder

---
+++ After80/PrimeNumMethod.java
@@ -0,0 +1,32 @@
+package After80;
+
+import java.util.Scanner;
+
+public class PrimeNumMethod {
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= n / 2; i++)
+            if (n % i == 0)
+
+                return false;
+        return true;
+
+    }
+
+    public static void printPrime(int bin, int end) {
+        for (int i = bin; i <= end; i++)
+            if (isPrime(i))
+                System.out.print(i + " ");
+
+    }
+
+    public static void main(String[] args) {
+        try (Scanner s = new Scanner(System.in)) {
+
+            printPrime(s.nextInt(), s.nextInt());
+            System.out.println();
+
+        }
+    }
+
+}
