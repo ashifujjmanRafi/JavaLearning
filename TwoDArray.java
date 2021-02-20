79b4c964cead1619653b02defb502359df6ebbd0
ashifujjmanRafi <ajrafi2722@gmail.com>
2021-02-21 00:06:36 +0600

    21;02

---
+++ TwoDArray.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+
+        try (Scanner s = new Scanner(System.in)) {
+
+            System.out.println("enter the value of collumb and row : ");
+
+            int row = s.nextInt();
+
+            var colloumb = s.nextInt();
+
+            int[][] array2 = new int[row][colloumb];
+
+            takeInput(array2, row, colloumb);
+
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < colloumb; j++) {
+
+                    System.out.print(array2[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+        }
+
+    }
+
+    private static void takeInput(int[][] array2, int row, int colloumb) {
+        Scanner s = new Scanner(System.in);
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < colloumb; j++) {
+                array2[i][j] = s.nextInt();
+
+            }
+
+        }
+        System.out.println();
+    }
+
+}
