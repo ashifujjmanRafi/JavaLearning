62a864f1a1e1c766bfb64354506715fecaf60a02
ashifujjmanRafi <ajrafi2722@gmail.com>
2021-03-02 20:11:05 +0600

    encapsulation

--- CircleEncapsulation.java
+++ CircleEncapsulation.java
@@ -1,12 +1,15 @@
 public class CircleEncapsulation {
     Point center;
+
     private double radius;
-    private static int numofcircle=0;
+
+    private static int numofcircle = 0;
+    //it cant be called by class name
 
     CircleEncapsulation(Point center, double radius) {
         numofcircle++;
-        
-		this.center = center;
+
+        this.center = center;
         this.radius = radius;
 
     }
@@ -19,24 +22,25 @@ public class CircleEncapsulation {
         return Math.PI * radius * radius;
     }
 
-    void setCenter(Point center){
+    void setCenter(Point center) {
         this.center = center;
 
     }
 
-    public double getRadius(){
+    public double getRadius() {
         return radius;
 
     }
-    public void setRadius(double rad){
-        this.radius=rad>=0?rad:0;
+
+    public void setRadius(double rad) {
+        this.radius = rad >= 0 ? rad : 0;
 
     }
-    //static variable method can be called by class name or obj 
-    public static int getNumofCircle(){
+
+    // static variable method can be called by class name or obj
+    public static int getNumofCircle() {
         return numofcircle;
 
     }
 
-    
 }
